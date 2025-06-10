96cdd8ca41c02243cec4ae9cfea8a570b051b72d
ryoKato <rkato81progrex@gmail.com>
2025-06-10 14:27:29 +0900

    修正です。

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,11 @@
+// tailwind.config.js
+const typography = require('@tailwindcss/typography');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md}"], // ← ここに md を追加
+  content: ["./src/**/*.{astro,html,js,jsx,ts,tsx,md}"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [typography],
 };
